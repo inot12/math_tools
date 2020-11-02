22937b1539e9dab3ddbfe501993caf9e2ae19619
Toni Jelusic <tjelusic18@gmail.com>
2020-11-02 16:45:42 +0100

    Newton-Raphson works for everything except trigonometric functions.

--- incremental_iterative_methods.py
+++ incremental_iterative_methods.py
@@ -3,31 +3,80 @@
 Created on Nov 2, 2020
 
 @author:toni
+
+A collection of mathematical tools used to obtain solutions by using the
+incremental-iterative approach.
+The function 'func' has to be modified to return your mathematical function
+every time you want to use Newton-Raphson to obtain its solution/roots.
 """
 
+import math
 import numpy as np
 import sympy as sp
 
 
 def func(x):
     """Return a mathematical function."""
-    return x
+    return math.e**(-x) - x
 
 
-def newton_raphson(f):
-    """
-    Return the solution of a function.
+def error(a, b):
+    """Return the relative error of two values.
     
-    f -- function
+    a -- float, reference value
+    b -- float, compared value
+    
+    returns: float, relative error
+    """
+    return abs(a-b)/abs(a)
+    
+
+def newton_raphson(f, xn):
+    """
+    Return the solution of a function by using Newton-Raphson method.
+    
+    f -- function object, mathematical function of ONE argument
+    xn -- float, current initial guess
+    
+    returns: float
+    
+    x_n+1 = x_n - f(x_n)/df(x_n)
+    """
+    x = sp.Symbol('x')
+    symbolic_f = f(x)
+    derivative_f = symbolic_f.diff(x)
+    df = sp.lambdify(x, derivative_f)
+    return xn - f(xn)/df(xn)
+
+
+def riks(f, xn):
+    """
+    Return the solution of a function by using Riks (Arc Length) method.
+    
+    f -- mathematical function of ONE argument
+    xn -- float, current initial guess
     
     returns: float
     """
-    
-    
-
-
-def main():
     pass
+
+
+def iterate(f, x0, method=newton_raphson, tol=1e-7):
+    """Return the solution of iteration procedure for the chosen method."""
+    while error(method(f, x0), x0) > tol:
+        x0 = method(f, x0)
+    return method(f, x0)
+
+
+def incremental(increment=0.1):
+    """Return the solution to a mathematical problem by using increments."""
+    pass
+    
+    
+def main():
+    print(newton_raphson(func, 0))
+    print(iterate(func, 0))
+    print(iterate(func, 0, method=riks))
 
 
 if __name__ == "__main__":
--- test_incremental_iterative_methods.py
+++ test_incremental_iterative_methods.py
@@ -4,105 +4,130 @@ Created on Nov 2, 2020
 
 @author:toni
 
-Test your script with the name <scriptname.py>.
-Name convention for the unit test file: <test_scriptname.py>
-Remove each unnecessary line with CTRL+D
 """
 
 import unittest
+import math
+import sympy as sp
 import incremental_iterative_methods as iim
 
 # from module import class  # import the class you are about to test
 # from module import function  # import the function you are about to test
 
 
-class TestMdl(unittest.TestCase):
-    """Describe the test case."""
-    
-    def test_function(self):
-        """Describe the test.
-        Methods are always named 'test_'+'function/class/method'."""
-        # good tests test for all the related cases
-        # good tests have more than one assert!
-        self.assertEqual(f(known_value), known_result)  # general case
-        self.assertEqual(f(known_value), known_result)  # edge case 1
-        self.assertEqual(f(known_value), known_result)  # edge case 2
-
-
-class ClassNameFunctionNameKnownValues(unittest.TestCase):
-    """Test for known values of class ClassName or function FunctionName."""
+class TestIncrementalIterativeMethod(unittest.TestCase):
+    """Test the module incremental_iterative_methods.py."""
     
     def setUp(self):
-        """Create an instance of the class for use in all test methods."""
-        # only used for classes; not required for testing functions
-        # setUp method runs BEFORE EVERY SINGLE TEST
-        # example:
-        # self.instance_name = Class(*args)
-        pass  # instance initialization comes here
-    
-    def tearDown(self):
-        """Destroy all instances of tested files for a clean state."""
-        # tearDown method runs AFTER EVERY SINGLE TEST
-        pass
-    
-    @classmethod  # we work with the class and not the instance of the class
-    def setUpClass(cls):
-        """RUNS BEFORE ANYTHING IN THE TEST."""
-        # use for costly operations that you want to do only once
-        # this does not run before every single test individually,
-        # but before all tests, and it does it only once
-        pass
-    
-    @classmethod
-    def tearDownClass(cls):
-        """RUNS AFTER ANYTHING IN THE TEST."""
-        pass
-    
-    def test_case1(self):  # test method names begin with 'test'
-        """f should return known results for known values"""
-        self.assertEqual(f(known_value), known_result)
+        def f(x):
+            return math.e**(-x) - x
+        self.f = f
         
-    def test_case2(self):
-        """f should return known results for known values"""
-        self.assertEqual(f(known_value), known_result)
+        def g(x):
+            return x**2 - 4*x - 5
+        self.g = g
+        
+        def h(x):
+            return x**3 - x - 1
+        self.h = h
+        
+        def k(x):
+            return math.cos(x) - 2*x
+        self.k = k
+    
+    def test_newton_raphson(self):
+        """Test the function newton_raphson"""
+        self.assertAlmostEqual(iim.newton_raphson(self.f, 0), 0.5)
+        
+    def test_iterate(self):
+        """Test the function iterate()."""
+        self.assertAlmostEqual(iim.iterate(self.f, 0), 0.5671, delta=1e-4)
+        self.assertAlmostEqual(iim.iterate(self.g, 1), -1, delta=1e-4)
+        self.assertAlmostEqual(iim.iterate(self.g, 10), 5, delta=1e-4)
+        self.assertAlmostEqual(iim.iterate(self.h, 1), 1.3247, delta=1e-4)
+        # this assertion fails because cosine function cannot be easily
+        # transformed from sympy object to lambda function
+        # How to transform function objects to sympy expressions and vice versa
+        # when you have sin() or cos() in your function?
+        # when you want to transform trigonometric functions?
+        # python3, sympy, lambdify
+        # How to lambdify sympy expressions that contain trigonometric functions?
+        # sympy is slow, that is why I am using lambdify.
+        self.assertAlmostEqual(iim.iterate(self.k, 0.5), 0.45018, delta=1e-4)
 
 
-class ClassNameFunctionNameBadInput(unittest.TestCase):
-    """Test for bad inputs of class ClassName or function FunctionName."""
-    
-    def setUp(self):
-        """Create an instance of the class for use in all test methods."""
-        # only used for classes; not required for testing functions
-        pass  # instance initialization comes here
-    
-    def test_case1(self):  # test method names begin with 'test'
-        """f should fail if condition"""
-        # we test excetpions by assertRaises
-        # assertRaises takes 3 arguments
-        # 1. The error that should be raised
-        # 2. The function or method that should raise it
-        # 3. All the arguments of mdl.f that result in the exception
-        # Example: divide(a,b) which divides two numbers
-        # We test for zero division error
-        # self.assertRaises(ValueError, mdl.divide, 5, 0)
-        self.assertRaises(ErrorType, mdl.f, *args)
-        
-    def test_case2(self):
-        """f should fail if condition"""
-        self.assertRaises(ErrorType, mdl.f, *args)
-        
-    def test_case3(self):
-        """Alternative with context manager.
-        Use if you have more exceptions of the same type."""
-        with self.assertRaises(ValueError):
-            calc.divide(10, 0)
-            calc.divide(10, '1')
+# class ClassNameFunctionNameKnownValues(unittest.TestCase):
+#     """Test for known values of class ClassName or function FunctionName."""
+#     
+#     def setUp(self):
+#         """Create an instance of the class for use in all test methods."""
+#         # only used for classes; not required for testing functions
+#         # setUp method runs BEFORE EVERY SINGLE TEST
+#         # example:
+#         # self.instance_name = Class(*args)
+#         pass  # instance initialization comes here
+#     
+#     def tearDown(self):
+#         """Destroy all instances of tested files for a clean state."""
+#         # tearDown method runs AFTER EVERY SINGLE TEST
+#         pass
+#     
+#     @classmethod  # we work with the class and not the instance of the class
+#     def setUpClass(cls):
+#         """RUNS BEFORE ANYTHING IN THE TEST."""
+#         # use for costly operations that you want to do only once
+#         # this does not run before every single test individually,
+#         # but before all tests, and it does it only once
+#         pass
+#     
+#     @classmethod
+#     def tearDownClass(cls):
+#         """RUNS AFTER ANYTHING IN THE TEST."""
+#         pass
+#     
+#     def test_case1(self):  # test method names begin with 'test'
+#         """f should return known results for known values"""
+#         self.assertEqual(f(known_value), known_result)
+#         
+#     def test_case2(self):
+#         """f should return known results for known values"""
+#         self.assertEqual(f(known_value), known_result)
+# 
+# 
+# class ClassNameFunctionNameBadInput(unittest.TestCase):
+#     """Test for bad inputs of class ClassName or function FunctionName."""
+#     
+#     def setUp(self):
+#         """Create an instance of the class for use in all test methods."""
+#         # only used for classes; not required for testing functions
+#         pass  # instance initialization comes here
+#     
+#     def test_case1(self):  # test method names begin with 'test'
+#         """f should fail if condition"""
+#         # we test excetpions by assertRaises
+#         # assertRaises takes 3 arguments
+#         # 1. The error that should be raised
+#         # 2. The function or method that should raise it
+#         # 3. All the arguments of mdl.f that result in the exception
+#         # Example: divide(a,b) which divides two numbers
+#         # We test for zero division error
+#         # self.assertRaises(ValueError, mdl.divide, 5, 0)
+#         self.assertRaises(ErrorType, mdl.f, *args)
+#         
+#     def test_case2(self):
+#         """f should fail if condition"""
+#         self.assertRaises(ErrorType, mdl.f, *args)
+#         
+#     def test_case3(self):
+#         """Alternative with context manager.
+#         Use if you have more exceptions of the same type."""
+#         with self.assertRaises(ValueError):
+#             calc.divide(10, 0)
+#             calc.divide(10, '1')
 
        
 def main():
-    # unittest.main() enables you to call the test from CLI as:
-    # $ python3 <test_scriptname.py>
-    unittest.main(verbosity=2)  # verbose output
+    unittest.main(verbosity=2)
 
 
 if __name__ == "__main__":
